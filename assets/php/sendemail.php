7f44cf086d9dc75eb2d16d06c324c2421000326e
Ezequiel Est <ezequielestigarribia@gmail.com>
2018-12-03 19:14:34 -0300

    No se envia email hasta verificacion

--- assets/php/sendemail.php
+++ assets/php/sendemail.php
@@ -11,19 +11,20 @@
 		(!isset($subject) || $subject == "") || (!isset($message) || $message == ""))
 	{
 		$respuesta['respuesta'] = "incomplete";
-	}
-
-	$email_from = $email;
-	$email_to = 'ezequiel.estigarribia@gmail.com'; //replace with your email
-
-	$body = 'Name: ' . $name . "\n\n" . 'Email: ' . $email . "\n\n" . 'Subject: ' . $subject . "\n\n" . 'Message: ' . $message;
-
-	$success = @mail($email_to, $body, 'Name: ' . $name . "\n\n" . 'Email: ' . $email . "\n\n" . 'Subject: ' . $subject . "\n\n" . 'Message: ' . $message);
-	
-	if ($success){
-		$respuesta['respuesta'] = "ok";
 	}else{
-		$respuesta['respuesta'] = "error";
+
+		$email_from = $email;
+		$email_to = 'ezequiel.estigarribia@gmail.com'; //replace with your email
+
+		$body = 'Name: ' . $name . "\n\n" . 'Email: ' . $email . "\n\n" . 'Subject: ' . $subject . "\n\n" . 'Message: ' . $message;
+
+		$success = @mail($email_to, $body, 'Name: ' . $name . "\n\n" . 'Email: ' . $email . "\n\n" . 'Subject: ' . $subject . "\n\n" . 'Message: ' . $message);
+		
+		if ($success){
+			$respuesta['respuesta'] = "ok";
+		}else{
+			$respuesta['respuesta'] = "error";
+		}
 	}
 
 	$res = json_encode($respuesta);
